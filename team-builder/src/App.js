9aa03bc2586a8f1d700e274666b4bd20f4a7d8d5
tania <tanislavens@yahoo.com>
2019-07-24 16:31:55 -0400

    wip: steps 1 and 2 complete

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,24 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+
 import './App.css';
 
 function App() {
+  const [userState, setUserState] = useState({
+    name: '',
+    email: '',
+    role: ''
+  });
+  const changeHandler = (event) => {
+    setUserState({ ...userState, [event.target.name]: event.target.value })
+    console.log('user', changeHandler, event.target)
+  }
+  const submitHandler = (event) => {
+    event.preventDefault()
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSumbit={submitHandler}>
+        <fieldset>
+          <legend>Add Team Member</legend>
+          <div>
+            <label>
+              Name
+            <input
+                name='username'
+                type='text'
+                placeholder='Enter username'
+                value={userState.username}
+                onChange={changeHandler}
+              />
+            </label>
+            <label>
+              Email
+            <input
+                name='email'
+                type='text'
+                placeholder='Enter email'
+                value={userState.email}
+                onChange={changeHandler}
+              />
+            </label>
+            <label>
+              Role
+            <input
+                name='role'
+                type='text'
+                placeholder='Enter role'
+                value={userState.role}
+                onChange={changeHandler}
+              />
+            </label>
+          </div>
+        </fieldset>
+      </form>
     </div>
   );
 }
